5362d012e471ca5eb6464ff89dd8d424843abb24
machamu-24 <masamu420@gmail.com>
2025-11-07 11:25:37 +0900

    deviseのストロングパラメーターを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
             username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
         end
     end
+
+    #deviseで新規登録時に追加カラムを許可
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [
+            :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday
+        ])
+    end
 end

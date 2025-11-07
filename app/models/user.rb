256b9de63b2cbd26a357ea94ba9df3cf23831f79
machamu-24 <masamu420@gmail.com>
2025-11-07 11:46:23 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,22 @@
 class User < ApplicationRecord
-  #deviseモジュール
+  # deviseモジュール
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #ニックネームは必須
+  # ニックネームは必須
   validates :nickname, presence: true
 
-  #パスワードは半角英数字混合で6文字以上
+  # パスワードは半角英数字混合で6文字以上
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' }
 
-  #名前(全角)
+  # 名前(全角)
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください'}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角文字を使用してください' }
 
-  #名前(カタカナ)
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'には全角カタカナを使用してください'}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'には全角カタカナを使用してください'}
+  # 名前(カタカナ)
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'には全角カタカナを使用してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'には全角カタカナを使用してください' }
 
-  #生年月日
+  # 生年月日
   validates :birthday, presence: true
 end
- 

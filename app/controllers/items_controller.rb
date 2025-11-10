2ae42c4d69dfbb49ed46e98ba39257afc239945a
machamu-24 <masamu420@gmail.com>
2025-11-10 22:05:21 +0900

    Itemscontrollerを作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,30 @@
+# app/controllers/items_controller.rb
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
+    @items = Item.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '出品が完了しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :category_id, :condition_id, :shipping_fee_status_id,
+      :prefecture_id, :shipping_day_id, :price, :image
+    ).merge(user_id: current_user.id)
   end
 end

501afa06620f9695ef9b62e6f3c2f150987adcc4
machamu-24 <masamu420@gmail.com>
2025-11-10 22:35:26 +0900

    Itemモデルの単体テストを実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,22 @@
+# spec/factories/items.rb
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
-    category_id { 1 }
-    condition_id { 1 }
-    shipping_fee_status_id { 1 }
-    prefecture_id { 1 }
-    shipping_day_id { 1 }
-    price { 1 }
-    user { nil }
+    name                  { 'テスト商品' }
+    description           { '説明テキスト' }
+    category_id           { 2 }
+    condition_id          { 2 }
+    shipping_fee_status_id { 2 }
+    prefecture_id         { 2 }
+    shipping_day_id       { 2 }
+    price                 { 1000 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(
+        io: File.open(Rails.root.join('spec/fixtures/files/test.png')),
+        filename: 'test.png',
+        content_type: 'image/png'
+      )
+    end
   end
 end

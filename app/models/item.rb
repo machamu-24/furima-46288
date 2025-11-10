d8925a87c7ce247ffcf1e6097dd1a8f18ffb7101
machamu-24 <masamu420@gmail.com>
2025-11-10 22:02:15 +0900

    Itemモデルの関連とバリデーションを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,40 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  # ActiveHash
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_status
+  belongs_to :shipping_day
+  belongs_to :prefecture
+
+  # バリデーション
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_status_id
+    validates :shipping_day_id
+    validates :prefecture_id
+  end
+
+  with_options numericality: { other_than: 1, message: 'を選択してください' } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_status_id
+    validates :shipping_day_id
+    validates :prefecture_id
+  end
+
+  validates :price,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 300,
+              less_than_or_equal_to: 9_999_999
+            },
+            format: { with: /\A[0-9]+\z/, message: '半角数字で入力してください' }
 end

0fb714e9f22064ef275e6f66286ca7ddcfc6b275
machamu-24 <masamu420@gmail.com>
2025-11-15 08:37:18 +0900

    テストを追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -90,5 +90,11 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include('Price は半角数字で入力してください')
     end
+
+    it 'userが紐づいていないと出品できない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include('User must exist')
+    end
   end
 end

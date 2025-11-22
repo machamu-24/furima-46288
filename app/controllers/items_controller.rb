ca2d5626100875064239f2b70fcbbd2bf8b8f5d2
machamu-24 <masamu420@gmail.com>
2025-11-22 14:50:12 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 # app/controllers/items_controller.rb
 class ItemsController < ApplicationController
-  # ログインしていないと　new/createにアクセスできない
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -20,6 +20,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
 
   # ストロングパラメータ
@@ -28,5 +31,9 @@ class ItemsController < ApplicationController
       :name, :description, :category_id, :condition_id, :shipping_fee_status_id,
       :prefecture_id, :shipping_day_id, :price, :image
     ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end

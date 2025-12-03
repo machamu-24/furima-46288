4f6b15e23db73ae1d9e4922d2e3b81399acb9bed
machamu-24 <masamu420@gmail.com>
2025-12-03 13:44:06 +0900

    ssl修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -32,11 +32,19 @@ class OrdersController < ApplicationController
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(
-      amount: @item.price,
-      card: order_params[:token],
-      currency: 'jpy'
-    )
+
+    begin
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+    rescue Payjp::APIConnectionError, OpenSSL::SSL::SSLError => e
+      
+      Rails.logger.error "PAY.JP connection error: #{e.class} #{e.message}"
+      
+    end
+
   end
 
   def move_to_root
